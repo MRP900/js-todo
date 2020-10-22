517a60a893c2e8111707326e0b7b050b67c7dada
unknown <mattrpittman@gmail.com>
2020-10-22 14:33:01 -0600

    added delete/styles/basic validation

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
 window.onload = ()=> {
     document.getElementById("add_btn").addEventListener("click", (e)=>{
         
-        var todo = document.getElementById("todo-text").value;
-        var date = document.getElementById("date").value;
-        var time = document.getElementById("time").value;
+        // select form inputs
+        var todo = document.getElementById("todo-text");
+        var date = document.getElementById("date");
+        var time = document.getElementById("time");
+       
+        var todos = document.getElementById("todos");
 
-        if(todo.lenth > 0) {
-            var node = document.createElement("li");
-            node.setAttribute("class", "todo");
-            var textNode = document.createTextNode(todo);
-            node.appendChild(textNode);
-            var deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
-            deleteButton.addEventListener('click', e => {
-                e.target.parentElement.remove()
-            })
-            node.appendChild(deleteButton);
+        console.log(todo.value);
+        console.log(date.value);
+        console.log(time.value);
+        
+        if((todo.value.length > 0) && (date.value.length > 0) && (time.value.length > 0)) {
+            // create todo 'li', set class, add todo text to element
+        var liNode = document.createElement("li");
+        liNode.setAttribute("class", "todo");
+
+        // Add todo text
+        var pNode = document.createElement("p");
+        var textNode = document.createTextNode(todo.value);
+
+        pNode.appendChild(textNode);
+        liNode.appendChild(pNode);
+        
+        // Add date
+        var dateNode = document.createElement("p");
+        var dateText = document.createTextNode(date.value);
+
+        dateNode.appendChild(dateText);
+        liNode.appendChild(dateNode);
+        
+        // Add time
+        var timeNode = document.createElement("p");
+        var timeText = document.createTextNode(time.value);
+
+        timeNode.appendChild(timeText);
+        liNode.appendChild(timeNode);
+
+        // add delete button to li
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener('click', e => {
+            e.target.parentElement.remove()
+        })
+        liNode.appendChild(deleteButton);
+        
+        // add the todo element to the container
+        todos.appendChild(liNode);
+
+        // reset iputs
+        todo.value = '';
+        date.value = '';
+        time.value = '';
         }
-
-        // alert(todo);
+        
+        
     })
 }
